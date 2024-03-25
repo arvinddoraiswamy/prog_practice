f37273b138d2d4d03de6b05b92d56a8d88d377a0
Arvind <arvind.doraiswamy@gmail.com>
2024-03-24 20:08:55 -0700

    nicer solution

--- leetcode/283.py
+++ leetcode/283.py
@@ -4,14 +4,13 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         ptr1 = 0
-        orig_len = len(nums)
-        ptr2 = orig_len
-        count = 0
-
-        while count < orig_len:
-            if nums[ptr1] == 0:
-                nums.append(nums[ptr2])
-                nums.pop(ptr1)
-            else:
-                ptr1 += 1
-            count += 1
+        ptr2 = 0
+        while ptr1 < len(nums):
+            if nums[ptr1] != 0:
+                nums[ptr2] = nums[ptr1]
+                ptr2 += 1
+            ptr1 += 1
+        
+        while ptr2 < len(nums):
+            nums[ptr2] = 0
+            ptr2 += 1
