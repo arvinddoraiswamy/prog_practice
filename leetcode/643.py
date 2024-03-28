e178d9c35c33866bb463a34d74d02af3830d3f2d
Arvind <arvind.doraiswamy@gmail.com>
2024-03-27 22:07:16 -0700

    max average subarray

--- leetcode/643.py
+++ leetcode/643.py
@@ -10,15 +10,18 @@ class Solution:
             int_sum = sum(nums[ptr1:ptr2+1])
             while ptr2 < len(nums):
                 avg = int_sum / k
+                # Works as long as average is positive
                 if avg > 0 and avg > max_avg:
                     max_avg = avg
                 #This logic shouldn't work, but negative numbers are for some reason treated as > 0 and this works
                 elif avg < 0:
-                    if max_avg == 0:
+                    if max_avg == 0: #For some reason although 0 > negative number, leet code expects
+                                     # the neg number as an answer hence this weird special case
                         max_avg = avg
-                    elif avg > max_avg:
+                    elif avg > max_avg: #Negative number comparison here
                         max_avg = avg
                 
+                #Sliding window movement. Subtract the 1st number before you move
                 int_sum -= nums[ptr1]
                 ptr1 += 1
                 ptr2 += 1
