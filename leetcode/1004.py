cbaec61d34f02705785a8fcd6511a6bdf995eb7a
Arvind <arvind.doraiswamy@gmail.com>
2024-03-31 12:30:52 -0700

    NON WORKING consecutive ones pushed

--- leetcode/1004.py
+++ leetcode/1004.py
@@ -2,30 +2,25 @@ class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         ptr1 = 0
         ptr2 = 0
-        current = zeros = ones = max_ones = 0
-        for ptr2 in range(0, len(nums)):
+        zc = max_ones = 0
+        while ptr2 < len(nums):
             if nums[ptr2] == 1:
-                ones += 1
-            elif (nums[ptr2] == 0 and zeros < k):
-                zeros += 1
-            else:
-                print("max reached. move ptr1")
-                current = ones + zeros
-                if nums[ptr1] == 0:
-                    zeros -= 1
+                ptr2 += 1
+            elif nums[ptr2] == 0:
+                if zc < k:
+                    zc += 1
+                    ptr2 += 1
                 else:
-                    ones -= 1
-                ptr1 += 1
-                zeros += 1
-                
-                if current > max_ones:
-                    max_ones = current
-                current = 0
-            print(f"Ptr1: {ptr1}, Ptr2: {ptr2}, Ones: {ones}, Zeros: {zeros}, Max: {max_ones}")
-    
-        print(f"Last: {ones} {zeros}")
-        current = ones+zeros
-        if current > max_ones:
-            max_ones = current
-
+                    # Reached max contiguous length possible
+                    current = ptr2 - ptr1
+                    if current > max_ones:
+                        max_ones = current
+                    while nums[ptr1] == 1:
+                        ptr1 += 1
+                    #The moment you hit a zero you're decreasing and increasing zc by 1, and moving ptr2
+                    #Effectively hence, zc -1 + 1 = zc. So just move ptr2 to the right by 1
+                    #Also it's a new start, so we have to reset our current count
+                    current = 0
+                    ptr2 += 1
+        
         return max_ones
